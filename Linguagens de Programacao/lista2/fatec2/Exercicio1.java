5162de8a84b9f9f13d1309c2ca8a5a4f553b5fa9
ricardothx <ricardothx@gmail.com>
2021-09-26 17:46:27 -0300

    novas classes adicionadas

--- Linguagens de Programacao/lista2/fatec2/Exercicio1.java
+++ Linguagens de Programacao/lista2/fatec2/Exercicio1.java
@@ -1,4 +1,4 @@
-package listaricardo;
+package fatec2;
 
 import java.util.ArrayList;
 import java.util.Scanner;
--- Linguagens de Programacao/lista2/fatec2/Exercicio4.java
+++ Linguagens de Programacao/lista2/fatec2/Exercicio4.java
@@ -1,4 +1,4 @@
-package listaricardo;
+package fatec2;
 
 import java.util.Scanner;
 
